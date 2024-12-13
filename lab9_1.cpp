14e579d7866a7ddc0c968bb6a9951f16c89d2652
aerithmygoat <gsticky@g>
2024-12-13 23:08:09 +0700

    lab9_1com

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,10 +2,15 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+    double pay,loan,interest,rate;
+    int i = 1;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +25,24 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	do{
+	    interest = ((loan*rate)/100);
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << interest;
+	loan += interest;
+	cout << setw(13) << left << loan;
+	if (loan <= pay){
+	    pay = loan;
+	    cout << setw(13) << left << pay;
+	}else{
+	    cout << setw(13) << left << pay;
+	}
+    loan -= pay;
+	cout << setw(13) << left << loan;
+	i++;
 	cout << "\n";	
-	
-	return 0;
+	}while(loan != 0);
+ return 0;
 }
